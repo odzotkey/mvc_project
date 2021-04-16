d72680472f1978ba9ae8eaf8fbb8a2c94cca8e54
web_developer <purnamaindah262@gmail.com>
2021-04-16 20:04:35 +0700

    update app.php
    debug fixed

--- system/core/App.php
+++ system/core/App.php
@@ -5,13 +5,17 @@ class App {
     protected $params = [];
     function __construct(){
         $url = $this->parse_url();
-         if(file_exists('application/controllers/'.$url[0].'.php')){
-            $this->controller = $url[0];
-            unset($url[0]);
-           
-         } else {
-         
-         }
+        
+        if (!empty($url)) {
+             if(file_exists('application/controllers/'.$url[0].'.php')){
+                $this->controller = $url[0];
+                unset($url[0]);
+               
+             } else {
+             
+             }
+        }
+
           require_once "application/controllers/".$this->controller.".php";
          $this->controller = new $this->controller();
          if(isset($url[1])){
@@ -29,22 +33,17 @@ class App {
     } 
 
      function parse_url(){
-         $url = parse_url($_GET["url"]);
-         $url = $url["path"];
-         $url = rtrim($url, "/");
-         $url = filter_var($url, FILTER_SANITIZE_URL);
-         $url = explode('/', $url);
+         $url = "";
+         if (!empty($_GET["url"])) {
+            $url = parse_url($_GET["url"]);
+             $url = $url["path"];
+             $url = rtrim($url, "/");
+             $url = filter_var($url, FILTER_SANITIZE_URL);
+             $url = explode('/', $url);
+         }
          return $url;
      }
     
-    
-     function p_url(){
-     /*
-     $url = rtrim($_GET['url']);
-     $url = filter_var($url, FILTER_SANITIZE_URL);
-     $url = explode('/', $url);
-     return $url;
-     */
-     }
+
    
 }
